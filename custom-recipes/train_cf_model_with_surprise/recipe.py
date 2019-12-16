a9782d464379d8c3a0f7322729e842d9cba3fb9e
Katie Gross <ktgross15@gmail.com>
2019-12-16 11:39:07 -0500

    12-16

---
+++ custom-recipes/train_cf_model_with_surprise/recipe.py
@@ -0,0 +1,318 @@
+import dataiku
+from dataiku.customrecipe import *
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+import datetime
+import ast
+import random
+import json
+import pickle 
+
+from surprise import SVD, SVDpp, NMF, KNNBaseline, KNNBasic, KNNWithMeans, KNNWithZScore, NormalPredictor, BaselineOnly, SlopeOne, CoClustering
+from surprise import Reader, Dataset
+from surprise.model_selection import train_test_split, cross_validate
+from surprise import accuracy
+from surprise.model_selection import GridSearchCV
+
+from dku_collab_filtering.algo_dictionary import *
+
+
+#### READ INPUT PARAMETERS ####
+def read_recipe_config(param):
+    param_val = get_recipe_config().get(param, '')
+    return param_val
+
+user_id_col = read_recipe_config('user_id_col')
+item_id_col = read_recipe_config('item_id_col')
+rating_col = read_recipe_config('rating_col')
+ratings_scale_min = read_recipe_config('ratings_scale_min')
+ratings_scale_max = read_recipe_config('ratings_scale_max')
+k_fold_splits = int(read_recipe_config('k_fold_splits'))
+error_metric = read_recipe_config('error_metric')
+
+svdpp_bool = read_recipe_config("svdpp")
+svd_bool = read_recipe_config("svd")
+nmf_bool = read_recipe_config("nmf")
+normalpredictor_bool = read_recipe_config("normalpredictor")
+baselineonly_bool = read_recipe_config("baselineonly")
+slopeone_bool = read_recipe_config("slopeone")
+coclustering_bool = read_recipe_config("coclustering")
+knnbasic_bool = read_recipe_config("knnbasic")
+knnbaseline_bool = read_recipe_config("knnbaseline")
+knnwithmeans_bool = read_recipe_config("knnwithmeans")
+knnwithzscore_bool = read_recipe_config("knnwithzscore")
+
+
+#### READ AND FORMAT RECIPE INPUTS ####
+
+input_dataset = dataiku.Dataset(get_input_names_for_role('input_dataset')[0])
+input_df = input_dataset.get_dataframe()
+input_df = input_df[[user_id_col, item_id_col, rating_col]]
+
+# read input data into Surprise
+reader = Reader(rating_scale = (ratings_scale_min, ratings_scale_max))
+data = Dataset.load_from_df(input_df, reader)
+
+
+### DEFINE FUNCTIONS ###
+
+# def add_param_to_algo_dict(algo_dictionary, algo_name, param, param_dict={}):    
+#     algo_param_plugin = '{}_{}'.format(algo_name, param)
+#     print "Adding parameters to algo dict:", param
+    
+#     # add parameter dictionary for sim or bsl options
+#     if param in ['sim_options', 'bsl_options']:
+#         print "adding parameter dictionary"
+#         algo_dictionary[algo_name]['grid_params'][param] = param_dict
+    
+#     # add all other parameters
+#     else:
+#         param_val = read_recipe_config(algo_param_plugin)
+#         if type(param_val) == bool:
+#             param_val = [param_val]
+#         elif (len(param_val) >= 1) & (param_val != "[]"):
+#             param_val = json.loads(param_val)
+#         algo_dictionary[algo_name]['grid_params'][param] = param_val
+    
+#     return algo_dictionary
+
+
+# def add_algo_to_algo_dict(algo_dictionary, algo_bool, algo_mod, algo_name, params=[], param_dicts=[]):
+#     if algo_bool:
+#         print "ADDING", algo_name
+#         # add algo to algo_dictionary
+#         algo_dictionary[algo_name] = {}
+#         algo_dictionary[algo_name]['grid_params'] = {}
+#         algo_dictionary[algo_name]['module'] = algo_mod
+        
+#         # add each parameter to the grid search parameters
+#         for param in params:
+#             print param
+#             algo_dictionary = add_param_to_algo_dict(algo_dictionary, algo_name, param)
+        
+#         # add parameter dictionaries if necessary
+#         if len(param_dicts) >= 1:
+#             for param_dict in param_dicts:
+#                 algo_dictionary = add_param_to_algo_dict(algo_dictionary, algo_name, param, param_dict)
+                
+#     return algo_dictionary
+
+# def generate_dict_from_params(algo_dictionary, algo_bool, algo_name, inner_params):
+#     param_dict = {}
+#     if algo_bool:
+#         for inner_param in inner_params:
+#             algo_param_plugin = '{}_{}'.format(algo_name, inner_param)
+#             param_val = read_recipe_config(algo_param_plugin)
+#             print "got param val", inner_param, param_val, type(param_val)
+            
+#             # if valid parameter_value is entered, add to parameter dict
+#             if type(param_val) == bool:
+#                 param_dict[inner_param] = [param_val]
+#             elif (len(param_val) >= 1) & (param_val != "[]"):
+#                 param_dict[inner_param] = [param_val]
+         
+#     return param_dict
+
+
+def dump_modified(folder, file_name, predictions=None, algo=None):
+    """A basic wrapper around Pickle to serialize a list of prediction and/or an algorithm on drive."""
+
+    dump_obj = {'predictions': predictions, 'algo': algo}
+    with folder.get_writer(file_name) as file_obj:
+        pickle.dump(dump_obj, file_obj, protocol=pickle.HIGHEST_PROTOCOL)
+      
+    
+def test_model_return_prediction_sets(model, train, test, error_metric):
+    test_preds = model.test(test)
+    
+    if error_metric == 'rmse':
+        test_score = accuracy.rmse(test_preds)
+    elif error_metric == 'mae':
+        test_score = accuracy.mae(test_preds)
+    elif error_metric == 'fcp':
+        test_score = accuracy.fcp(test_preds)
+    
+    train_set_ready = train.build_testset()
+    train_preds = model.test(train_set_ready)
+
+    return test_preds, test_score, train_preds
+
+
+#### GENERATE ALGO DICTIONARY WITH PARAMETERS FOR EACH ALGO INCLUDED ####
+
+algo_dictionary = {}
+
+# generate parameter dictionaries where necessary
+baselineonly_bsl_dict = generate_dict_from_params(algo_dictionary, baselineonly_bool, 'baselineonly', ['method'])
+knnbaseline_bsl_dict = generate_dict_from_params(algo_dictionary, knnbaseline_bool, 'knnbaseline', ['method'])
+knnbasic_sim_options_dict = generate_dict_from_params(algo_dictionary, knnbasic_bool, 'knnbasic', ['name', 'user_based'])
+knnbaseline_sim_options_dict = generate_dict_from_params(algo_dictionary, knnbaseline_bool, 'knnbaseline', ['name', 'user_based'])
+knnwithmeans_sim_options_dict = generate_dict_from_params(algo_dictionary, knnwithmeans_bool, 'knnwithmeans', ['name', 'user_based'])
+knnwithzscore_sim_options_dict = generate_dict_from_params(algo_dictionary, knnwithzscore_bool, 'knnwithzscore', ['name', 'user_based'])
+
+# generate algorithm dictionaries
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, svd_bool, SVD, 'svd', ['n_factors', 'n_epochs', 'lr_all', 'reg_all', 'biased'])
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, svdpp_bool, SVDpp, 'svdpp', ['n_factors', 'n_epochs', 'lr_all', 'reg_all'])
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, nmf_bool, NMF, 'nmf', ['n_factors', 'n_epochs','biased'])
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, normalpredictor_bool, NormalPredictor, 'normalpredictor')
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, slopeone_bool, SlopeOne, 'slopeone')
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, coclustering_bool, CoClustering, 'coclustering', ['n_cltr_u', 'n_cltr_i', 'n_epochs'])
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, baselineonly_bool, BaselineOnly, 'baselineonly', ['bsl_options'], [baselineonly_bsl_dict])
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, knnbasic_bool, KNNBasic, 'knnbasic', ['k', 'min_k', 'sim_options'], [knnbasic_sim_options_dict])
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, knnbaseline_bool, KNNBaseline, 'knnbaseline', ['k', 'min_k', 'sim_options', 'bsl_options'], [knnbaseline_sim_options_dict, knnbaseline_bsl_dict])
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, knnwithmeans_bool, KNNWithMeans, 'knnwithmeans', ['k', 'min_k', 'sim_options'], [knnwithmeans_sim_options_dict])
+algo_dictionary = add_algo_to_algo_dict(algo_dictionary, knnwithzscore_bool, KNNWithZScore, 'knnwithzscore', ['k', 'min_k', 'sim_options'], [knnwithzscore_sim_options_dict])
+
+
+
+#### MODEL TRAINING & GRID SEARCH ####
+
+# train test split
+raw_ratings = data.raw_ratings
+random.shuffle(raw_ratings)
+
+threshold = int(.8 * len(raw_ratings)) # A = 80% of the data, B = 20% of the data
+A_raw_ratings = raw_ratings[:threshold]
+B_raw_ratings = raw_ratings[threshold:]
+
+data.raw_ratings = A_raw_ratings
+test_set = data.construct_testset(B_raw_ratings)  # testset is now the set B
+train_set = data.build_full_trainset()
+
+
+# generate models folder
+folder_name = get_output_names_for_role('grid_search_models')[0]
+surprise_cv_models = dataiku.Folder(folder_name)
+
+# instantiate empty trained_models dictionary and master_cv_df dataframe
+trained_models = {}
+master_cv_df = pd.DataFrame()
+
+# for each algorithm in the user-input list, train a model and add prediction datasets/error metrics to a master trained-models dictionary
+all_algos = algo_dictionary.keys()
+for algo in all_algos:
+    model_module = algo_dictionary[algo]['module']
+    param_grid = algo_dictionary[algo]['grid_params']
+    print(model_module)
+    print(param_grid)
+    
+    gs = GridSearchCV(model_module, param_grid, measures=[error_metric], cv=k_fold_splits, n_jobs=2, joblib_verbose=15)
+    gs.fit(data)
+    
+    for estimator in gs.best_estimator.items():
+        print(estimator)
+        
+        # retrain on the whole set A
+        model = estimator[1]
+        model.fit(train_set)
+
+        model_file_name = algo + '_best_model.pkl'
+        dump_modified(surprise_cv_models, model_file_name, algo=model)   
+        
+        model_test_preds, model_test_score, model_train_preds = test_model_return_prediction_sets(model, train_set, test_set, error_metric)
+        
+        name = algo + estimator[0]
+        trained_models[name] = {}
+        trained_models[name]['model'] = model
+        trained_models[name]['model_test_preds'] = model_test_preds
+        trained_models[name]['model_test_score'] = model_test_score
+        trained_models[name]['model_train_preds'] = model_train_preds
+            
+    gs_results_df = pd.DataFrame.from_dict(gs.cv_results)
+    gs_results_df['algorithm'] = algo
+    
+    print('Done with CV for algorithm: ' + algo)
+    master_cv_df = master_cv_df.append(gs_results_df)
+       
+# modify gs output
+# sort by error metric col and add new cols
+metric_rank_col = 'rank_test_{}'.format(error_metric)
+metric_val_col = 'mean_test_{}'.format(error_metric)
+overall_col = 'overall_{}_rank'.format(error_metric)
+metric_rank_within_algo = '{}_rank_within_algo'.format(error_metric)
+
+master_cv_df = master_cv_df.sort_values(by=metric_val_col)
+master_cv_df['added_to_models_folder'] = np.where(master_cv_df[metric_rank_col]==1, True, False)
+master_cv_df = master_cv_df.rename(columns= {metric_rank_col: metric_rank_within_algo})
+master_cv_df[overall_col] = master_cv_df[metric_val_col].rank()
+
+# reorder gs dataframe col order
+time_cols = []
+error_cols = []
+param_cols = []
+other_cols = []
+
+cols = master_cv_df.columns
+for col in cols:
+    if "time" in col:
+        time_cols.append(col)
+    elif (("rmse" in col) | ("mae" in col) | ("fcp" in col)):
+        error_cols.append(col)
+    elif "param" in col:
+        param_cols.append(col)
+    elif "algorithm" not in col:
+        other_cols.append(col)
+
+new_cols =  ['algorithm'] + param_cols + error_cols + time_cols + other_cols
+master_cv_df = master_cv_df[new_cols]
+
+# select the best model and get preds and score
+best_model = min(trained_models, key=lambda x: trained_models[x].get('model_test_score'))
+train_preds = trained_models[best_model]['model_train_preds']
+test_preds = trained_models[best_model]['model_test_preds']
+test_score = trained_models[best_model]['model_test_score']
+
+
+##### FORMAT PREDICTION DATASETS ####
+
+def create_ratings_df(preds, u_id_col, i_id_col):
+    iids = []
+    uids = []
+    act_rrs = []
+    est_rrs = []
+
+    for row in preds:
+        iids.append(row.iid)
+        uids.append(row.uid)
+        act_rrs.append(row.r_ui)
+        est_rrs.append(row.est)
+
+    preds_df = pd.DataFrame(columns=[u_id_col,i_id_col,'actual_rr','pred_rr'])
+    preds_df[u_id_col] = uids
+    preds_df[i_id_col] = iids
+    preds_df['actual_rr'] = act_rrs
+    preds_df['pred_rr'] = est_rrs
+    return preds_df
+
+def get_pivoted_pred_act_dfs(surprise_algo_preds, u_id_col, i_id_col):
+    # input is the best model's predictions, and pivots it to have one row per user and one column per item with user/item predictions filling such columns
+    master_df = create_ratings_df(surprise_algo_preds, u_id_col, i_id_col)
+    predictions_df_pivoted = master_df.pivot(index=u_id_col, columns=i_id_col, values='pred_rr')
+    actual_df_pivoted = master_df.pivot(index=u_id_col, columns=i_id_col, values='actual_rr')
+
+    return predictions_df_pivoted, actual_df_pivoted
+
+def merge_preds_actual_dfs(pred_df, actual_df):
+    pred_df.columns = [str(col) + '_pred' for col in pred_df.columns]
+    actual_df.columns = [str(col) + '_actual' for col in actual_df.columns]
+    full_df = pd.merge(pred_df, actual_df, left_index=True, right_index=True)
+    return full_df
+
+# generate pivoted dataframes with preds and actuals for both train and test
+test_preds_df, test_actuals_df = get_pivoted_pred_act_dfs(test_preds, user_id_col, item_id_col)
+train_preds_df, train_actuals_df = get_pivoted_pred_act_dfs(train_preds, user_id_col, item_id_col)
+
+# merge preds and actuals to get compairison dataframes 
+test_preds_actuals = merge_preds_actual_dfs(test_preds_df, test_actuals_df).reset_index()
+train_preds_actuals = merge_preds_actual_dfs(train_preds_df, train_actuals_df).reset_index()
+
+    
+#### WRITE RECIPE OUTPUTS ####
+surprise_cv_grid_search_results = dataiku.Dataset(get_output_names_for_role("grid_search_error_metrics")[0])
+surprise_cv_grid_search_results.write_with_schema(master_cv_df)
+
+train_preds_actuals_dataset = dataiku.Dataset(get_output_names_for_role("train_preds_actuals")[0])
+train_preds_actuals_dataset.write_with_schema(train_preds_actuals)
+
+test_preds_actuals_dataset = dataiku.Dataset(get_output_names_for_role("test_preds_actuals")[0])
+test_preds_actuals_dataset.write_with_schema(test_preds_actuals)
---
+++ python-lib/dku_collab_filtering/algo_dictionary.py
@@ -0,0 +1,59 @@
+import json
+
+
+def add_param_to_algo_dict(algo_dictionary, algo_name, param, param_dict={}):    
+    algo_param_plugin = '{}_{}'.format(algo_name, param)
+    print "Adding parameters to algo dict:", param
+    
+    # add parameter dictionary for sim or bsl options
+    if param in ['sim_options', 'bsl_options']:
+        print "adding parameter dictionary"
+        algo_dictionary[algo_name]['grid_params'][param] = param_dict
+    
+    # add all other parameters
+    else:
+        param_val = read_recipe_config(algo_param_plugin)
+        if type(param_val) == bool:
+            param_val = [param_val]
+        elif (len(param_val) >= 1) & (param_val != "[]"):
+            param_val = json.loads(param_val)
+        algo_dictionary[algo_name]['grid_params'][param] = param_val
+    
+    return algo_dictionary
+
+
+def add_algo_to_algo_dict(algo_dictionary, algo_bool, algo_mod, algo_name, params=[], param_dicts=[]):
+    if algo_bool:
+        print "ADDING", algo_name
+        # add algo to algo_dictionary
+        algo_dictionary[algo_name] = {}
+        algo_dictionary[algo_name]['grid_params'] = {}
+        algo_dictionary[algo_name]['module'] = algo_mod
+        
+        # add each parameter to the grid search parameters
+        for param in params:
+            print param
+            algo_dictionary = add_param_to_algo_dict(algo_dictionary, algo_name, param)
+        
+        # add parameter dictionaries if necessary
+        if len(param_dicts) >= 1:
+            for param_dict in param_dicts:
+                algo_dictionary = add_param_to_algo_dict(algo_dictionary, algo_name, param, param_dict)
+                
+    return algo_dictionary
+
+def generate_dict_from_params(algo_dictionary, algo_bool, algo_name, inner_params):
+    param_dict = {}
+    if algo_bool:
+        for inner_param in inner_params:
+            algo_param_plugin = '{}_{}'.format(algo_name, inner_param)
+            param_val = read_recipe_config(algo_param_plugin)
+            print "got param val", inner_param, param_val, type(param_val)
+            
+            # if valid parameter_value is entered, add to parameter dict
+            if type(param_val) == bool:
+                param_dict[inner_param] = [param_val]
+            elif (len(param_val) >= 1) & (param_val != "[]"):
+                param_dict[inner_param] = [param_val]
+         
+    return param_dict
